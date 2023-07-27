f59792324dedf5fa8d5fa1abc647be66b0db3aec
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2023-07-27 18:45:27 +0700

    update is finished quiz history

--- model/quizHistory.go
+++ model/quizHistory.go
@@ -9,4 +9,5 @@ type QuizHistory struct {
 	QuizID      uint   `json:"quiz_id" form:"quiz_id" gorm:"primary_key"`
 	AttemptDate string `json:"attempt_date" form:"attempt_date"`
 	Score       int64  `json:"score" form:"score" gorm:"default:0"`
+	IsFinished  bool   `json:"is_finished" form:"is_finished" gorm:"default:false"`
 }
